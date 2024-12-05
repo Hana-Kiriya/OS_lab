c8e64dd5ad7570a3d704d88753f390ff0815ccd4
hana <aksvtina3@gmail.com>
2024-12-05 21:40:20 +0800

    success

--- LAB3/LAB3/3/3_1/My_Kernel.c
+++ LAB3/LAB3/3/3_1/My_Kernel.c
@@ -4,6 +4,7 @@
 #include <linux/printk.h>
 #include <linux/proc_fs.h>
 #include <asm/current.h>
+#include <asm/uaccess.h>
 
 #define procfs_name "Mythread_info"
 #define BUFSIZE  1024
@@ -16,8 +17,38 @@ static ssize_t Mywrite(struct file *fileptr, const char __user *ubuf, size_t buf
 
 
 static ssize_t Myread(struct file *fileptr, char __user *ubuf, size_t buffer_len, loff_t *offset){
+    //offset:偏移量，ubuf:用戶空間的緩衝區，buffer_len:緩衝區大小
     /*Your code here*/
+    struct task_struct *thread;
+    int len = 0; //計算緩衝區的偏移量
 
+    if(*offset > 0){ //偏移不為0，表已讀取過
+        return 0;
+    }
+
+    for_each_thread(current, thread){ //歷遍所有執行緒
+        len += snprintf(buf + len, BUFSIZE - len, "PID: %d, TID: %d, Priority: %d, State: %d\n", current -> pid, thread -> pid, thread -> prio, thread -> state);
+
+        if(len >= BUFSIZE){ //緩衝區滿了，截斷資料並停止寫入
+            pr_warn("Buffer overflow in Myread.\n");
+            len = BUFSIZE;
+            break;
+        }
+    }
+
+    if(len > buffer_len){ //確保不超過用戶空間大小
+        len = buffer_len;
+    }
+
+    int ret = copy_to_user(ubuf, buf, len);
+    if(ret != 0){ //若複製成功則 = 0，if不成立，反之失敗則回傳未被成功複製的字節數量，ex:欲複製6個字節，僅4個字節成功複製，則copy_to_user返還2，因2 != 0，故if成立，執行if內指令
+        pr_err("Failed to copy data to user space.\n");
+        return -EFAULT;
+    }
+
+    *offset += len;
+
+    return len;
     /****************/
 }
 
