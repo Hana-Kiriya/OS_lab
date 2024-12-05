5523b1cff7eb150e2967422a3ea71d67b1c32118
Hana-Kiriya <138017691+Hana-Kiriya@users.noreply.github.com>
2024-12-06 02:45:55 +0800

    Update My_Kernel.c

--- LAB3/LAB3/3/3_2/My_Kernel.c
+++ LAB3/LAB3/3/3_2/My_Kernel.c
@@ -32,67 +32,18 @@ static ssize_t Mywrite(struct file *fileptr, const char __user *ubuf, size_t buf
 }
 
 
-static ssize_t Myread(struct file *fileptr, char __user *ubuf, size_t buffer_len, loff_t *offset){
+static ssize_t Myread(struct file *fileptr, char __user *ubuf, size_t buffer_len, loff_t *offset) {
     size_t len = 0; // Calculate buffer length
     if (*offset > 0) { // If offset is non-zero, that means it's already read
         return 0;
     }
 
-    // Read the content of the /proc/Mythread_info file to check how many lines there are
-    struct file *proc_file = filp_open("/proc/Mythread_info", O_RDONLY, 0);
-    if (IS_ERR(proc_file)) {
-        pr_err("Failed to open /proc/Mythread_info\n");
-        return -EFAULT;
-    }
+    // Instead of reading /proc/Mythread_info again, you can just append messages directly
+    len += snprintf(buf + len, BUFSIZE - len, "Thread 1 says hello!\n");
 
-    char file_contents[BUFSIZE];
-    loff_t pos = 0;
-    ssize_t bytes_read = kernel_read(proc_file, file_contents, BUFSIZE, &pos);
-
-    if (bytes_read < 0) {
-        pr_err("Failed to read /proc/Mythread_info\n");
-        filp_close(proc_file, NULL);
-        return -EFAULT;
-    }
-
-    file_contents[bytes_read] = '\0'; // Null-terminate the string
-
-    // Count the number of lines in the file content
-    int line_count = 0;
-    char *line = file_contents;
-    while (line && *line) {
-        if (*line == '\n') {
-            line_count++;
-        }
-        line++;
-    }
-
-    // If there's only one line in the file
-    if (line_count == 1) {
-        len += snprintf(buf + len, BUFSIZE - len, "%s\n", file_contents);
-        len += snprintf(buf + len, BUFSIZE - len, "Thread 1 says hello!\n");
-        if(len >= BUFSIZE){ //緩衝區滿了，截斷資料並停止寫入
-            pr_warn("Buffer overflow in Myread.\n");
-            len = BUFSIZE;
-        }
-    } 
-    // If there are two lines in the file
-    else if (line_count == 2) {
-        char *first_line = file_contents;
-        char *second_line = strchr(file_contents, '\n') + 1;
-
-        // First line + "Thread 1 says hello"
-        len += snprintf(buf + len, BUFSIZE - len, "%s\n", first_line);
-        len += snprintf(buf + len, BUFSIZE - len, "Thread 1 says hello!\n");
-
-        // Second line + "Thread 2 says hello"
-        len += snprintf(buf + len, BUFSIZE - len, "%s\n", second_line);
-        len += snprintf(buf + len, BUFSIZE - len, "Thread 2 says hello!\n");
-        if(len >= BUFSIZE){ //緩衝區滿了，截斷資料並停止寫入
-            pr_warn("Buffer overflow in Myread.\n");
-            len = BUFSIZE;
-        }
-    }
+#if defined(THREAD_NUMBER) && THREAD_NUMBER == 2
+    len += snprintf(buf + len, BUFSIZE - len, "Thread 2 says hello!\n");
+#endif
 
     // Ensure buffer length doesn't exceed the provided size
     if (len > buffer_len) {
@@ -102,14 +53,13 @@ static ssize_t Myread(struct file *fileptr, char __user *ubuf, size_t buffer_len
     int ret = copy_to_user(ubuf, buf, len);
     if (ret != 0) {
         pr_err("Failed to copy data to user space.\n");
-        filp_close(proc_file, NULL);
         return -EFAULT;
     }
 
     *offset += len;
-    filp_close(proc_file, NULL);
     return len;
 }
+
 
 static struct proc_ops Myops = {
     .proc_read = Myread,
