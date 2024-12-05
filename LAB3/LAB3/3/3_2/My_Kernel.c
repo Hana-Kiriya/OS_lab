7615d0c9a537722d64b69ccbe34dbb57b1ed02c3
hana <aksvtina3@gmail.com>
2024-12-06 02:19:02 +0800

    1

--- LAB3/LAB3/3/3_2/My_Kernel.c
+++ LAB3/LAB3/3/3_2/My_Kernel.c
@@ -33,41 +33,74 @@ static ssize_t Mywrite(struct file *fileptr, const char __user *ubuf, size_t buf
 
 
 static ssize_t Myread(struct file *fileptr, char __user *ubuf, size_t buffer_len, loff_t *offset){
-    //offset:偏移量，ubuf:用戶空間的緩衝區，buffer_len:緩衝區大小
-    /*Your code here*/
-
-
-    size_t len = 0; //計算緩衝區的偏移量
-
-    if(*offset > 0){ //偏移不為0，表已讀取過
+    size_t len = 0; // Calculate buffer length
+    if (*offset > 0) { // If offset is non-zero, that means it's already read
         return 0;
     }
 
-    for_each_thread(current, thread){
-        len += snprintf(buf + len, BUFSIZE - len, "%s\n", data);
-        len += snprintf(buf + len, BUFSIZE - len, "PID: %d, TID: %d, time: %d, State: %d\n", current -> tgid, current -> pid, current -> utime/100/1000);
-
-        if(len >= BUFSIZE){ //緩衝區滿了，截斷資料並停止寫入
-            pr_warn("Buffer overflow in Myread.\n");
-            len = BUFSIZE;
-            break;
-        }
+    // Read the content of the /proc/Mythread_info file to check how many lines there are
+    struct file *proc_file = filp_open(PROC_FILE_PATH, O_RDONLY, 0);
+    if (IS_ERR(proc_file)) {
+        pr_err("Failed to open /proc/Mythread_info\n");
+        return -EFAULT;
     }
-    
-    if(len > buffer_len){ //確保不超過用戶空間大小
+
+    char file_contents[BUFSIZE];
+    loff_t pos = 0;
+    ssize_t bytes_read = kernel_read(proc_file, file_contents, BUFSIZE, &pos);
+
+    if (bytes_read < 0) {
+        pr_err("Failed to read /proc/Mythread_info\n");
+        filp_close(proc_file, NULL);
+        return -EFAULT;
+    }
+
+    file_contents[bytes_read] = '\0'; // Null-terminate the string
+
+    // Count the number of lines in the file content
+    int line_count = 0;
+    char *line = file_contents;
+    while (line && *line) {
+        if (*line == '\n') {
+            line_count++;
+        }
+        line++;
+    }
+
+    // If there's only one line in the file
+    if (line_count == 1) {
+        len += snprintf(buf + len, BUFSIZE - len, "%s\n", file_contents);
+        len += snprintf(buf + len, BUFSIZE - len, "%s\n", THREAD_1_MSG);
+    } 
+    // If there are two lines in the file
+    else if (line_count == 2) {
+        char *first_line = file_contents;
+        char *second_line = strchr(file_contents, '\n') + 1;
+
+        // First line + "Thread 1 says hello"
+        len += snprintf(buf + len, BUFSIZE - len, "%s\n", first_line);
+        len += snprintf(buf + len, BUFSIZE - len, "%s\n", THREAD_1_MSG);
+
+        // Second line + "Thread 2 says hello"
+        len += snprintf(buf + len, BUFSIZE - len, "%s\n", second_line);
+        len += snprintf(buf + len, BUFSIZE - len, "%s\n", THREAD_2_MSG);
+    }
+
+    // Ensure buffer length doesn't exceed the provided size
+    if (len > buffer_len) {
         len = buffer_len;
     }
 
     int ret = copy_to_user(ubuf, buf, len);
-    if(ret != 0){ //若複製成功則 = 0，if不成立，反之失敗則回傳未被成功複製的字節數量，ex:欲複製6個字節，僅4個字節成功複製，則copy_to_user返還2，因2 != 0，故if成立，執行if內指令
+    if (ret != 0) {
         pr_err("Failed to copy data to user space.\n");
+        filp_close(proc_file, NULL);
         return -EFAULT;
     }
 
     *offset += len;
-
+    filp_close(proc_file, NULL);
     return len;
-    /****************/
 }
 
 static struct proc_ops Myops = {
